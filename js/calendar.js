20ac0b86941bfa351f3a9fc5ef46bb4685cd9859
JonDmcg <38740045+JonDmcg@users.noreply.github.com>
2020-12-16 15:36:47 -0800

    calendar

---
+++ js/calendar.js
@@ -0,0 +1,9 @@
+$('.head th.w').html('w');
+$('.head th.pi').html('pi');
+$('.head th.d0').html('M');
+$('.head th.d1').html('T');
+$('.head th.d2').html('W');
+$('.head th.d3').html('Th');
+$('.head th.d4').html('F');
+$('.head th.d5').html('Sa');
+$('.head th.d6').html('Su');
